4b4618cc37d533fce4f4e4656920ab884046a937
Song <2106220855@qq.com>
2023-01-11 14:35:00 +0800

    主界面

--- app/src/main/java/com/example/anli/MainActivity.java
+++ app/src/main/java/com/example/anli/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.anli;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initViewClick();
+    }
+
+    //初始化单击监听事件
+    protected void initViewClick(){
+        findViewById(R.id.AnLi_1).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.AnLi_1:
+                startActivity(new Intent(this,DrawerActivity.class));
+                break;
+        }
     }
 }
